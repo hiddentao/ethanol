447035bd408da826f2516ceb0ad37a830f605973
Ramesh Nair <ram@hiddentao.com>
2016-10-12 08:23:27 +0100

    yarn lock file

--- frontend/src/js/ui/components/accountList.js
+++ frontend/src/js/ui/components/accountList.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 import React from 'react';
 import Account from './account';
+import { connectRedux } from './helpers/decorators';
 import { css } from 'aphrodisiac';
 import styles from '../styles/components/accountList';
 
 
+@connectRedux()
 export default class Component extends React.Component {
   render () {
     const accounts = _.map(this.props.accounts, (data, id) => 
@@ -14,11 +16,11 @@ export default class Component extends React.Component {
     return (
       <div className={css(styles.main)}>
         {accounts}
+        <div className={css(styles.addForm)}>
+          <button>Add acccount</button>
+        </div>
       </div>
     );
   }
 }
-
-
-
 
